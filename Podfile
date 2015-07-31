7105ae596239cfc79c6f561b7688dba5d06a0d45
Chang <shuchang@Changs-Mac.local>
2015-07-30 20:11:45 -0400

    init n-Discovery

---
+++ Podfile
@@ -0,0 +1,4 @@
+platform :ios, '8.0'
+use_frameworks!
+pod 'MapboxGL'
+pod 'MMDrawerController', '~> 0.5.7'
---
+++ n-Discovery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  n-Discovery
+//
+//  Created by Chang on 7/30/15.
+//  Copyright (c) 2015 Kent State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
